55e6f7cbd63eb63429070be8b2195809b050e5c2
Oleksandr Kharchenko <harley029@gmail.com>
2024-03-11 15:31:23 +0100

    11.03.2024

--- from_lecturer/Lab (1).py
+++
@@ -1,9 +0,0 @@
-#line="Просто рядок тексту"
-try:
-    with open("f:\\файл.txt","r") as file1:
-        lines=file1.read().split("\n")
-        print(lines[2].split(" "))
-
-        
-except Exception as e:
-    print(f"Error: {e}")    
--- from_lecturer/zip_lab.py
+++
@@ -1,6 +0,0 @@
-from zipfile import ZipFile as zp
-try:
-    with zp("f:\\files.zip", mode="a") as archive:
-        archive.write("F:\\file.txt","F:\\file1.txt")
-except FileExistsError:
-    print("File exists")
